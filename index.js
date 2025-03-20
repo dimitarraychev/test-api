899569233cd69b3174569b9d59eb9c772541e829
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-03-20 15:33:35 +0200

    9

--- index.js
+++ index.js
@@ -13,17 +13,38 @@ app.post("/", (req, res) => {
   console.log("Body:");
   console.log(req.body);
 
-  const { command } = req.body;
-
   let response = {};
 
-  // if (command === "get_account_balance")
-  response = {
-    currency: "EUR",
-    totalbalance: totalbalance,
-    response_message: "ok",
-    response_code: "ok",
-  };
+  try {
+    const payload = JSON.parse(req.body.payload_json);
+    const command = payload.command;
+
+    console.log(`Received Command: ${command}`);
+
+    switch (command) {
+      case "get_account_balance":
+        response = {
+          currency: "EUR",
+          totalbalance: totalbalance,
+          response_message: "ok",
+          response_code: "ok",
+        };
+
+        res.json(response);
+        break;
+
+      case "add_account_game_win":
+        res.json({ message: "Game win logic here" });
+        break;
+
+      default:
+        console.log("Unknown command received:", command);
+        res.json({ message: "Unknown command" });
+    }
+  } catch (error) {
+    console.error("Error parsing payload_json:", error);
+    res.status(400).json({ message: "Invalid JSON format" });
+  }
 
   res.json(response);
 });

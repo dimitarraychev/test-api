6b9cfe3ffdaa42d5ac8d6c52a72a282017be2f50
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-03-20 14:03:33 +0200

    1

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let totalbalance = 99880;
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { command } = req.body;
+
+  let response = {};
+
+  if (command === "get_account_balance")
+    response = {
+      currency: "EUR",
+      totalbalance: totalbalance,
+      response_message: "ok",
+      response_code: "ok",
+    };
+
+  res.json(response);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

28d307321f5297fdf598b9fc9c21c74a05c5bb69
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-03-20 16:27:21 +0200

    14

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let totalbalance = 10000;
+let totalbalance = 100000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,47 +11,41 @@ app.post("/", (req, res) => {
   console.log("Body:");
   console.log(req.body);
 
-  let response = {};
+  const response = {
+    currency: "EUR",
+    response_message: "ok",
+    response_code: "ok",
+  };
 
   try {
     const payload = JSON.parse(req.body.payload_json);
     const command = payload.command;
+    const amount = payload.amount;
 
     console.log(`Received Command: ${command}`);
 
     switch (command) {
       case "get_account_balance":
-        response = {
-          currency: "EUR",
-          totalbalance: totalbalance,
-          response_message: "ok",
-          response_code: "ok",
-        };
+        response.totalbalance = totalbalance;
 
         res.json(response);
         break;
 
       case "add_account_game_bet":
-        const amount = payload.amount;
+        response.totalbalance = totalbalance - amount;
 
-        response = {
-          currency: "EUR",
-          totalbalance: totalbalance - amount,
-          response_message: "ok",
-          response_code: "ok",
-        };
+        res.json(response);
+        break;
+
+      case "add_account_game_win":
+        response.totalbalance = totalbalance + amount;
 
         res.json(response);
         break;
 
       case "cancel":
-        response = {
-          currency: "EUR",
-          totalbalance: totalbalance,
-          response_message: "ok",
-          response_code: "ok",
-          freeround_limit: 0,
-        };
+        response.totalbalance = totalbalance;
+        response.freeround_limit = 0;
 
         res.json(response);
         break;
@@ -69,5 +63,5 @@ app.post("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}...`);
 });

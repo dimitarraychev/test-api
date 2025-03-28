e81b96d3a9f0c731f4c3d8654a76500d2701db9e
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-03-28 14:06:46 +0200

    changed balance

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let totalBalance = 100000;
+let totalBalance = 123.456;
 
 app.use(express.urlencoded({ extended: true }));
 

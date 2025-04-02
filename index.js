1fb366c8df1f6c910ead060dcafdb6183861bb66
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-04-02 16:07:15 +0300

    added simulate err and cors

--- index.js
+++ index.js
@@ -1,47 +1,78 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let totalBalance = 123456;
-
+app.use(cors);
 app.use(express.urlencoded({ extended: true }));
+
+let totalBalance = 123456;
+const simulateError = { isActive: false, command_to_fail: "" };
+const response = {
+  currency: "EUR",
+  response_message: "ok",
+  response_code: "ok",
+};
 
 app.post("/", (req, res) => {
   console.log("Body:");
   console.log(req.body);
 
-  const response = {
-    currency: "EUR",
-    response_message: "ok",
-    response_code: "ok",
-  };
-
   try {
     const payload = JSON.parse(req.body.payload_json);
-    const { command, amount, bet_amount, win_amount } = payload;
+    const { command, amount, bet_amount, win_amount, command_to_fail } =
+      payload;
 
     console.log(`Received Command: ${command}`);
 
     switch (command) {
+      case "simulate_error":
+        simulateError.isActive = true;
+        simulateError.command_to_fail = command_to_fail;
+
+        res.json(response);
+        break;
+
       case "get_account_balance":
         response.totalbalance = totalBalance;
+
+        if (
+          simulateError.isActive &&
+          simulateError.command_to_fail === command
+        ) {
+          response.response_code = "error";
+          simulateError = { isActive: false, command_to_fail: "" };
+        }
 
         res.json(response);
         break;
 
       case "add_account_game_bet":
         totalBalance -= amount;
-
         response.totalbalance = totalBalance;
-        // response.response_code = "error";
+
+        if (
+          simulateError.isActive &&
+          simulateError.command_to_fail === command
+        ) {
+          response.response_code = "error";
+          simulateError = { isActive: false, command_to_fail: "" };
+        }
 
         res.json(response);
         break;
 
       case "add_account_game_win":
         totalBalance += amount;
-
         response.totalbalance = totalBalance;
+
+        if (
+          simulateError.isActive &&
+          simulateError.command_to_fail === command
+        ) {
+          response.response_code = "error";
+          simulateError = { isActive: false, command_to_fail: "" };
+        }
 
         res.json(response);
         break;
@@ -49,17 +80,31 @@ app.post("/", (req, res) => {
       case "add_account_game_bet_and_win":
         totalBalance -= bet_amount;
         totalBalance += win_amount;
-
         response.totalbalance = totalBalance;
+
+        if (
+          simulateError.isActive &&
+          simulateError.command_to_fail === command
+        ) {
+          response.response_code = "error";
+          simulateError = { isActive: false, command_to_fail: "" };
+        }
 
         res.json(response);
         break;
 
       case "cancel":
         totalBalance += amount;
-
         response.freeround_limit = 0;
         response.totalbalance = totalBalance;
+
+        if (
+          simulateError.isActive &&
+          simulateError.command_to_fail === command
+        ) {
+          response.response_code = "error";
+          simulateError = { isActive: false, command_to_fail: "" };
+        }
 
         res.json(response);
         break;

26fe7517195837fad10a1d09082ab3be40459574
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-04-02 16:17:26 +0300

    added json support

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let totalBalance = 123456;

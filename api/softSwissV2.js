3854d84222c076baf98b6f4432a733698c5418d6
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-04-04 15:19:01 +0300

    5235

--- api/softSwissV2.js
+++ api/softSwissV2.js
@@ -4,10 +4,17 @@ const router = express.Router();
 
 let totalBalance = 123456;
 
-router.post("/provider_a8r.Player/Balance", (req, res) => {
+// router.post("/provider_a8r.Player/Balance", (req, res) => {
+//   generateLogsSoftSwissV2(req);
+
+//   res.json({ message: "Balance retrieved", balance: totalBalance });
+// });
+
+router.post("/:subroute(*)", (req, res) => {
   generateLogsSoftSwissV2(req);
 
-  res.json({ message: "Balance retrieved", balance: totalBalance });
+  const { subroute } = req.params;
+  res.json({ message: `Matched subroute: ${subroute}`, balance: 1000 });
 });
 
 module.exports = router;
